c4acf34e5678fc03980d1f25210f9cd469789c70
Aleksey Belapko <alex.belapko@gmail.com>
2025-01-12 01:02:31 +0300

    implement DI config

--- src/Config/ContainerConfig.php
+++ src/Config/ContainerConfig.php
@@ -2,15 +2,35 @@
 
 namespace App\Config;
 
-use App\Controller\VideoController;
+use Psr\Container\ContainerInterface;
+use Slim\Container;
+use App\Cloud\YandexCloudStorageService;
 use App\Service\VideoService;
-use App\Repository\RabbitMQRepository;
-use App\VideoConverter;
-use DI\ContainerBuilder;
+use App\Converter\VideoConverter;
 
 class ContainerConfig
 {
-    public static function configure(ContainerBuilder $containerBuilder)
+    public static function configure(ContainerInterface $container)
     {
+        // Получаем конфигурацию
+        $config = require __DIR__ . '/../Config/parameters.php';
+
+        // Регистрация сервисов в контейнере
+        $container[VideoConverter::class] = function () {
+            return new VideoConverter();
+        };
+
+        $container[YandexCloudStorageService::class] = function () use ($config) {
+            return new YandexCloudStorageService($config['yandex']);
+        };
+
+        $container[VideoService::class] = function () use ($container, $config) {
+            return new VideoService(
+                $container->get(YandexCloudStorageService::class),
+                $container->get(VideoConverter::class),
+                $config['upload_path'],
+                $config['convert_path']
+            );
+        };
     }
 }
